b34955cf9a3329674933e901ac510f656b342963
filipaalima <139570730+filipalimaa@users.noreply.github.com>
2025-08-24 19:57:24 +0100

    fix: replace for ' in sql database

--- ts-postgres-docker/db/init/02_seed.sql
+++ ts-postgres-docker/db/init/02_seed.sql
@@ -1,10 +1,10 @@
 INSERT INTO people (name) VALUES
-    ("Teresa"),
-    ("Maria"),
-    ("Filipa"),
-    ("Luísa"),
-    ("António"),
-    ("Josefina"),
-    ("Clara"),
-    ("José"),
-    ("Manel");
+    ('Teresa'),
+    ('Maria'),
+    ('Filipa'),
+    ('Luísa'),
+    ('António'),
+    ('Josefina'),
+    ('Clara'),
+    ('José'),
+    ('Manel');
